3ba2569feb4c4fa335f0ee73ac928d95d53e12be
Ayowolemi Eni-omosule <ayowolemieniomosule@gmail.com>
2024-06-18 16:46:20 -0400

    I created the product model

---
+++ server/server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const PORT = process.env.PORT || 4000;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://ayowolemieniomosule:Ayowolemi%4021@marketplace.iimdtae.mongodb.net/?retryWrites=true&w=majority&appName=marketplace'
+const mongoose = require('mongoose')
+const express = require('express');
+
+const app = express()
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({ message: 'This is my first dress store' });
+});
+
+mongoose.connect(MONGODB_URI)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`App listening on port ${PORT} and this app has been connected to MongoDB`);
+        });
+    })
+    .catch((error) => {
+        console.error('Failed to connect to MongoDB:', error);
+    });
